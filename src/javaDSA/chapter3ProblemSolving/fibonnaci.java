35a5036f737bbf0823c6f0946b896ebf75cd7948
SarabjitS <sab.walia@gmail.com>
2023-08-29 22:31:37 -0230

    fibonacci series

--- src/javaDSA/chapter3ProblemSolving/fibonnaci.java
+++ src/javaDSA/chapter3ProblemSolving/fibonnaci.java
@@ -6,16 +6,16 @@ public class fibonnaci {
         Scanner sc = new Scanner(System.in);
         int firstNumber = 0;
         int secondNumber = 1;
-        System.out.println("The "+ firstNumber + "th number in Fibonnaci series is "+firstNumber);
-        System.out.println("The "+ secondNumber + "st number in Fibonnaci series is "+secondNumber);
+        System.out.println("The " + firstNumber + "th number in Fibonnaci series is " + firstNumber);
+        System.out.println("The " + secondNumber + "st number in Fibonnaci series is " + secondNumber);
         System.out.print("Enter the number of fibonnaci series ");
         int number = sc.nextInt();
         int sum = 0;
-        for(int i = 2; i<=number; i++){
+        for (int i = 2; i <= number; i++) {
             sum = firstNumber + secondNumber;
             firstNumber = secondNumber;
             secondNumber = sum;
         }
         System.out.println("The " + number + "number in series is   " + sum);
-
+    }
 }

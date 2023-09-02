83d6d00642a7ac7f107a14a358ef9409291a768c
SarabjitS <sab.walia@gmail.com>
2023-09-01 22:33:53 -0230

    questions added

---
+++ src/javaDSA/chapter2Loops/printAllPrimes.java
@@ -0,0 +1,21 @@
+package javaDSA.chapter2Loops;
+import java.util.Scanner;
+public class printAllPrimes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        int low = sc.nextInt();
+        int high = sc.nextInt();
+        for(int i = low ; i<=high; i++){
+            boolean isPrime = true;
+            for(int j=2; j*j<=i; j++){
+                if(i%j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(i);
+            }
+        }
+    }
+}
